bcc7e01ae496f9a4cd428c10a60a3dd324f50b13
ccc-goto <42729329+ccc-goto@users.noreply.github.com>
2021-10-18 13:07:43 +0900

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,14 @@
+﻿<?php
+$dsn = 'mysql:host=localhost;dbname=lesson1;charset=utf8';
+$db_user = 'root';
+$db_pass = '';
+ 
+try {
+    $pdo = new PDO($dsn, $db_user, $db_pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo '接続に成功しています';
+} catch (PDOException $e) {
+    print('接続失敗:'.$e->getMessage());
+    die();
+}
+
