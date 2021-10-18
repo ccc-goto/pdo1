fb1c61ad60e50821ccedbf7d1d162fe226c1ef77
ccc-goto <42729329+ccc-goto@users.noreply.github.com>
2021-10-18 15:35:39 +0900

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,11 @@
-﻿<?php
-$dsn = 'mysql:dbname=lesson1;host=localhost;charset=utf8';
-$db_user = 'root';
-$db_pass = '';
- 
+<?php
+$host = 'localhost';          //'127.0.0.1';
+$db = 'lesson1';              //データベース名
+$db_user = 'root';            //MySQL接続ユーザー名
+$db_pass = '';                //MySQL接続パスワード
+$charset = 'utf8mb4';         //文字コード
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";   //DSN
+
 try {
     $pdo = new PDO($dsn, $db_user, $db_pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -11,4 +14,3 @@ try {
     print('接続失敗:'.$e->getMessage());
     die();
 }
-
