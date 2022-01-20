498427ef9a35cc3b970a016a803f85b6b9c1f8cc
ccc-goto <42729329+ccc-goto@users.noreply.github.com>
2022-01-20 12:43:00 +0900

    aa

--- connect.php
+++ connect.php
@@ -9,7 +9,7 @@ $dsn = "mysql:host=$host;dbname=$db;charset=$charset";   //DSN
 try {
     $pdo = new PDO($dsn, $db_user, $db_pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo '接続に成功しています';
+    echo '接続に成功しています ';
 } catch (PDOException $e) {
     print('接続失敗:'.$e->getMessage());
     die();
